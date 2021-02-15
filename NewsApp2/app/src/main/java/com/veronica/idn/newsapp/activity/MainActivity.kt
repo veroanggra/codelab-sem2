d33d1beaa0f93ce6bbe0eece1b64eb8ffea7a016
veroanggra <veronicaputri497@gmail.com>
2021-02-15 12:02:42 +0700

    main

--- NewsApp2/app/src/main/java/com/veronica/idn/newsapp/activity/MainActivity.kt
+++ NewsApp2/app/src/main/java/com/veronica/idn/newsapp/activity/MainActivity.kt
@@ -7,8 +7,23 @@ import android.os.Bundle
 import android.view.View
 import com.veronica.idn.newsapp.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    val date = getCurrentDateTime()
+
+    fun getCurrentDateTime(): Date {
+        return Calendar.getInstance().time
+    }
+    fun Date.toString(format : String, locale : Locale = Locale.getDefault()):String{
+        val formatter = SimpleDateFormat (format, locale)
+        return formatter.format(this)
+    }
+
     companion object{
         fun getLaunchService (from: Context) = Intent(from, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
@@ -19,12 +34,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
         ib_profile_main.setOnClickListener(this)
+        tv_date_main.text = date.toString("dd/MM/yyyy")
     }
 
     override fun onClick(v: View) {
         when(v.id){
             R.id.ib_profile_main -> startActivity(Intent(ProfileActivity.getLaunchService(this)))
         }
-
     }
 }
---
+++ NewsApp2/app/src/main/java/com/veronica/idn/newsapp/utils/Const.kt
@@ -0,0 +1,5 @@
+package com.veronica.idn.newsapp.utils
+
+object Const {
+    val BASE_URL = "https://newsapi.org/v2/"
+}
